e1ae27f80bcee8e251ff260930ac643e078d863b
se7enmiles <seven.miles.x@gmail.com>
2021-04-01 01:07:18 +0400

    added PHP Docs and Autoload to front controller

--- index.php
+++ index.php
@@ -1,35 +1,32 @@
 <?php
 
 /**
- * Front controller
- * takes all requests
+ * Front controller, takes all requests
  */
 
 /**
  * Global Settings
- * are used all over the project
  */
 
 /**
  * Display errors
- * comment to disable showing errors
  */
-ini_set('display_errors',1);
+ini_set('display_errors',0);
 error_reporting(E_ALL);
+
+/**
+ * Start session
+ */
+session_start();
 
 /**
  * Attach system files
  */
 define('ROOT', dirname(__FILE__));
-require_once (ROOT.'/components/Router.php');
-require_once (ROOT.'/components/Database.php');
+require_once(ROOT.'/components/Autoload.php');
 
 /**
- * Database connection
- */
-
-/**
- * Call the router
+ * Call the Router
  */
 $router = new Router();
 $router->run();
